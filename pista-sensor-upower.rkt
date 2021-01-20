b772a2f6be448d7b34fd985633a40e0fc990f991
Siraaj Khandkar <siraaj@khandkar.net>
2021-01-20 14:56:50 -0500

    Re-indent unless

--- pista-sensor-upower.rkt
+++ pista-sensor-upower.rkt
@@ -46,101 +46,101 @@
 (define (loop input s0)
   (define line (read-line input))
   (unless (eof-object? line)
-          (let* ([fields (string-split line)]
-                 [p0 (state-parsing s0)]
-                 [s1
-                   (cond
-                     ; BOM when --dump
-                     [(regexp-match? #rx"^Device:[ \t]+" line)
-                      (struct-copy state s0 [parsing (device (second fields))])]
+    (let* ([fields (string-split line)]
+           [p0 (state-parsing s0)]
+           [s1
+             (cond
+               ; BOM when --dump
+               [(regexp-match? #rx"^Device:[ \t]+" line)
+                (struct-copy state s0 [parsing (device (second fields))])]
 
-                     ; BOM when --monitor-detail
-                     [(regexp-match?
-                        #rx"^\\[[0-9]+:[0-9]+:[0-9]+\\.[0-9]+\\][ \t]+device changed:[ \t]+"
-                        line)
-                      (struct-copy state s0 [parsing (device (fourth fields))])]
+               ; BOM when --monitor-detail
+               [(regexp-match?
+                  #rx"^\\[[0-9]+:[0-9]+:[0-9]+\\.[0-9]+\\][ \t]+device changed:[ \t]+"
+                  line)
+                (struct-copy state s0 [parsing (device (fourth fields))])]
 
-                     ; EOM
-                     [(regexp-match? #rx"^$" line)
-                      (log-debug "msg: ~v" p0)
-                      ; Use DisplayDevice if we have it, but do not stash it,
-                      ; because:
-                      ; 1. it is already an aggregate;
-                      ; 2. it is not reported by "--monitor-detail" - we expect
-                      ;    to see it only once, because of the initial "--dump".
-                      (let* ([s1
-                               (cond [(and (battery? p0)
-                                           ; Ignoring DisplayDevice:
-                                           (not (display-device? (battery-path p0))))
-                                      (struct-copy state s0
-                                                   [batteries
-                                                     (dict-set (state-batteries s0)
-                                                               (battery-path p0)
-                                                               p0)])]
-                                     [(line-power? p0)
-                                      (struct-copy state s0
-                                                   [plugged-in?
-                                                     (match (line-power-online p0)
-                                                       ["yes" #t]
-                                                       ["no"  #f])])]
-                                     [else s0])]
-                             [batteries
-                               ; Using DisplayDevice aggregate instead of computing our own:
-                               (if (and (battery? p0)
-                                        (display-device? (battery-path p0)))
-                                   (list p0)
-                                   (dict-values (state-batteries s1)))])
-                            (with-handlers
-                              ; Expect broken pipes
-                              ([exn:fail:filesystem:errno?
-                                 (λ (e) (log-error "Print failed: ~v\n" e))])
-                              (displayln (aggregate (state-plugged-in? s1) batteries))
-                              (flush-output))
-                            (struct-copy state s1 [parsing #f]))]
+               ; EOM
+               [(regexp-match? #rx"^$" line)
+                (log-debug "msg: ~v" p0)
+                ; Use DisplayDevice if we have it, but do not stash it,
+                ; because:
+                ; 1. it is already an aggregate;
+                ; 2. it is not reported by "--monitor-detail" - we expect
+                ;    to see it only once, because of the initial "--dump".
+                (let* ([s1
+                         (cond [(and (battery? p0)
+                                     ; Ignoring DisplayDevice:
+                                     (not (display-device? (battery-path p0))))
+                                (struct-copy state s0
+                                             [batteries
+                                               (dict-set (state-batteries s0)
+                                                         (battery-path p0)
+                                                         p0)])]
+                               [(line-power? p0)
+                                (struct-copy state s0
+                                             [plugged-in?
+                                               (match (line-power-online p0)
+                                                 ["yes" #t]
+                                                 ["no"  #f])])]
+                               [else s0])]
+                       [batteries
+                         ; Using DisplayDevice aggregate instead of computing our own:
+                         (if (and (battery? p0)
+                                  (display-device? (battery-path p0)))
+                             (list p0)
+                             (dict-values (state-batteries s1)))])
+                      (with-handlers
+                        ; Expect broken pipes
+                        ([exn:fail:filesystem:errno?
+                           (λ (e) (log-error "Print failed: ~v\n" e))])
+                        (displayln (aggregate (state-plugged-in? s1) batteries))
+                        (flush-output))
+                      (struct-copy state s1 [parsing #f]))]
 
-                     ; -- BEGIN battery
-                     [(and (device? p0)
-                           (regexp-match? #rx"^  battery$" line))
-                      (define p1 (battery (device-path p0) #f #f #f #f))
-                      (struct-copy state s0 [parsing p1])]
+               ; -- BEGIN battery
+               [(and (device? p0)
+                     (regexp-match? #rx"^  battery$" line))
+                (define p1 (battery (device-path p0) #f #f #f #f))
+                (struct-copy state s0 [parsing p1])]
 
-                     [(and (battery? p0)
-                           (regexp-match? #rx"^    state:" line))
-                      (define p1 (struct-copy battery p0 [state (second fields)]))
-                      (struct-copy state s0 [parsing p1])]
+               [(and (battery? p0)
+                     (regexp-match? #rx"^    state:" line))
+                (define p1 (struct-copy battery p0 [state (second fields)]))
+                (struct-copy state s0 [parsing p1])]
 
-                     [(and (battery? p0)
-                           (regexp-match? #rx"^    energy:" line))
-                      (define e (string->number (second fields)))
-                      (define p1 (struct-copy battery p0 [energy e]))
-                      (struct-copy state s0 [parsing p1])]
+               [(and (battery? p0)
+                     (regexp-match? #rx"^    energy:" line))
+                (define e (string->number (second fields)))
+                (define p1 (struct-copy battery p0 [energy e]))
+                (struct-copy state s0 [parsing p1])]
 
-                     [(and (battery? p0)
-                           (regexp-match? #rx"^    energy-full:" line))
-                      (define ef (string->number (second fields)))
-                      (define p1 (struct-copy battery p0 [energy-full ef]))
-                      (struct-copy state s0 [parsing p1])]
+               [(and (battery? p0)
+                     (regexp-match? #rx"^    energy-full:" line))
+                (define ef (string->number (second fields)))
+                (define p1 (struct-copy battery p0 [energy-full ef]))
+                (struct-copy state s0 [parsing p1])]
 
-                     [(and (battery? p0)
-                           (regexp-match? #rx"^    percentage:" line))
-                      (define pct (second fields))
-                      (define p1 (struct-copy battery p0 [percentage pct]))
-                      (struct-copy state s0 [parsing p1])]
-                     ; -- END battery
+               [(and (battery? p0)
+                     (regexp-match? #rx"^    percentage:" line))
+                (define pct (second fields))
+                (define p1 (struct-copy battery p0 [percentage pct]))
+                (struct-copy state s0 [parsing p1])]
+               ; -- END battery
 
-                     ; -- BEGIN line-power
-                     [(and (device? p0) (regexp-match? #rx"^  line-power$" line))
-                      (define dp (device-path p0))
-                      (struct-copy state s0 [parsing (line-power dp #f)])]
+               ; -- BEGIN line-power
+               [(and (device? p0) (regexp-match? #rx"^  line-power$" line))
+                (define dp (device-path p0))
+                (struct-copy state s0 [parsing (line-power dp #f)])]
 
-                     [(and (line-power? p0) (regexp-match? #rx"^    online:" line))
-                      (define o (second fields))
-                      (define p1 (struct-copy line-power p0 [online o]))
-                      (struct-copy state s0 [parsing p1])]
-                     ; -- END line-power
+               [(and (line-power? p0) (regexp-match? #rx"^    online:" line))
+                (define o (second fields))
+                (define p1 (struct-copy line-power p0 [online o]))
+                (struct-copy state s0 [parsing p1])]
+               ; -- END line-power
 
-                     [else s0])])
-                (loop input s1))))
+               [else s0])])
+          (loop input s1))))
 
 (define (start-logger level)
   (define logger (make-logger #f #f level #f))
