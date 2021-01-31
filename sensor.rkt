c036dd4f030037f4fac0bd028ed57d4b98081bb5
Siraaj Khandkar <siraaj@khandkar.net>
2021-01-30 22:58:57 -0500

    Comment on broken pipes and the need for retry

--- sensor.rkt
+++ sensor.rkt
@@ -7,7 +7,16 @@
 
 (define/contract (sensor:print/retry payload [init-backoff 1])
   (-> string? void?)
-  ; We expect occasional broken pipes:
+  ; Q: Why do we expect print failures?
+  ; A: We expect our stdout to be redirected to a FIFO, which is then read by
+  ; pista, which closes the pipe between message reads. We therefore expect
+  ; occasional broken pipes.
+  ;   UPower monitor is especially prone to encounter broken pipes, because
+  ; often the updates will come in bursts from UPower and pista will close the
+  ; pipe after reading the first message, which maybe while the subsequent ones
+  ; from the burst are still being written.
+  ;
+  ; Perhaps pista should allow more than a single message before pipe closure?
   (let retry ([backoff init-backoff])
     (with-handlers
       ([exn?
