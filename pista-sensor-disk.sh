46fdfd5f5849039568dc68bfa83047fbcc205543
Siraaj Khandkar <siraaj@khandkar.net>
2022-09-30 18:43:37 -0400

    Support prefix in disk sensor

--- pista-sensor-disk.sh
+++ pista-sensor-disk.sh
@@ -1,9 +1,12 @@
-#! /bin/sh
+#! /bin/bash
 
 trap '' PIPE
 
+interval="${1-5}"
+prefix="${2-d}"
+
 while :
 do
-    df ~ | awk 'NR == 2 {sub("%$", "", $5); printf "d %3d%%\n", $5}'
-    sleep "$1"
+    df ~ | awk -v prefix="$prefix" 'NR == 2 {sub("%$", "", $5); printf "%s%3d%%\n", prefix, $5}'
+    sleep "$interval"
 done
