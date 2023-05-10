a031645a984450a7c40fad9e2dd62fabc6d97361
Siraaj Khandkar <siraaj@khandkar.net>
2023-05-10 17:49:50 -0400

    Allow customizing on|off symbols in mic feed

--- src/bin/pista-feed-mic.rs
+++ src/bin/pista-feed-mic.rs
@@ -6,14 +6,24 @@ use pista_feeds::pulseaudio::Event;
 
 #[derive(Parser, Debug)]
 struct Cli {
-    // "i" as in "input" as in "input sink"
-    #[clap(long = "prefix", default_value = "i ")]
+    // "i" as in "input"
+    #[clap(long = "prefix", default_value = "i")]
     prefix: String,
+
+    #[clap(long = "symbol-on", default_value = "!")]
+    symbol_on: String,
+
+    #[clap(long = "symbol-off", default_value = " ")]
+    symbol_off: String,
 }
 
-fn print<W: std::io::Write>(mut buf: W, prefix: &str, num: usize) {
-    let symbol = if num > 0 { "!" } else { "_" };
-    if let Err(e) = { writeln!(buf, "{}{}", prefix, symbol) } {
+fn print<W: std::io::Write>(mut buf: W, cli: &Cli, num: usize) {
+    let symbol = if num > 0 {
+        &cli.symbol_on
+    } else {
+        &cli.symbol_off
+    };
+    if let Err(e) = { writeln!(buf, "{}{}", &cli.prefix, symbol) } {
         tracing::error!("Failed to write to stdout: {:?}", e)
     }
 }
@@ -24,7 +34,7 @@ fn main() -> Result<()> {
     tracing::info!("cli: {:?}", &cli);
     let mut sources = HashSet::new();
     let mut stdout = std::io::stdout().lock();
-    print(&mut stdout, &cli.prefix, sources.len());
+    print(&mut stdout, &cli, sources.len());
     for event_result in pista_feeds::pulseaudio::source_outputs_list()?
         .chain(pista_feeds::pulseaudio::source_outputs_subscribe()?)
     {
@@ -39,7 +49,7 @@ fn main() -> Result<()> {
                 tracing::error!("Failed to read event: {:?}", e)
             }
         }
-        print(&mut stdout, &cli.prefix, sources.len());
+        print(&mut stdout, &cli, sources.len());
         tracing::debug!("Sources: {:?}", &sources)
     }
     Err(anyhow!("Unexpected exit of 'pactl subscribe'"))
