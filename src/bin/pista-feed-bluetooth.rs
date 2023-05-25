e7b3fd0a327c6dffb9e5440f2bf074d50f6fb4b5
Siraaj Khandkar <siraaj@khandkar.net>
2023-05-25 18:30:15 -0400

    Refactor bluetooth with the new pipeline pattern

--- src/bin/pista-feed-bluetooth.rs
+++ src/bin/pista-feed-bluetooth.rs
@@ -1,9 +1,9 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use clap::Parser;
 
-use pista_feeds::bluetooth::DeviceState;
-
-#[derive(Debug, clap::Parser)]
+#[derive(Debug, Parser)]
 struct Cli {
     #[clap(long = "interval", short = 'i', default_value = "2.0")]
     interval: f64,
@@ -16,25 +16,8 @@ fn main() -> Result<()> {
     pista_feeds::log::init()?;
     let cli = Cli::parse();
     tracing::info!("cli: {:?}", &cli);
-    let interval = std::time::Duration::from_secs_f64(cli.interval);
-    let mut stdout = std::io::stdout().lock();
-    loop {
-        match DeviceState::read() {
-            Err(e) => {
-                tracing::error!(
-                    "Failed to get bluetooth device state: {:?}",
-                    e
-                );
-            }
-            Ok(None) => {
-                tracing::warn!("Did not find a bluetooth device");
-            }
-            Ok(Some(state)) => {
-                if let Err(e) = { state.write(&mut stdout, &cli.prefix) } {
-                    tracing::error!("Failed to write to stdout: {:?}", e);
-                }
-            }
-        }
-        std::thread::sleep(interval);
-    }
+    pista_feeds::bluetooth::run(
+        &cli.prefix,
+        Duration::from_secs_f64(cli.interval),
+    )
 }
--- src/lib/bluetooth/mod.rs
+++ src/lib/bluetooth/mod.rs
@@ -1,9 +1,46 @@
-use std::fs;
+use std::{fs, time::Duration};
 
 use anyhow::{anyhow, Result};
 
+struct State<'a> {
+    prefix: &'a str,
+    device_state: Option<DeviceState>,
+}
+
+impl<'a> State<'a> {
+    fn new(prefix: &'a str) -> Self {
+        Self {
+            prefix,
+            device_state: None,
+        }
+    }
+}
+
+impl<'a> crate::State for State<'a> {
+    type Msg = Option<DeviceState>;
+
+    fn update(
+        &mut self,
+        msg: Self::Msg,
+    ) -> Result<Option<Vec<Box<dyn crate::Alert>>>> {
+        self.device_state = msg;
+        Ok(None)
+    }
+
+    fn display<W: std::io::Write>(&self, mut buf: W) -> Result<()> {
+        // TODO Distinguish between OffSoft and OffHard
+        let symbol = match self.device_state {
+            Some(DeviceState::NoDev) | None => "--",
+            Some(DeviceState::On) => "on",
+            Some(DeviceState::OffSoft | DeviceState::OffHard) => "off",
+        };
+        writeln!(buf, "{}{}", self.prefix, symbol)?;
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
-pub enum DeviceState {
+enum DeviceState {
     NoDev,
     OffHard,
     OffSoft,
@@ -13,10 +50,10 @@ pub enum DeviceState {
 impl DeviceState {
     // TODO Lookup number of connected devices.
 
-    pub fn read() -> Result<Option<Self>> {
+    fn read() -> Result<Option<Self>> {
         // This method of device state lookup is taken from TLP bluetooth command.
         // TODO Checkout https://crates.io/crates/bluer
-        let mut bt_state: Option<Self> = None;
+        let mut bt_state_opt: Option<Self> = None;
         for entry in fs::read_dir("/sys/class/rfkill/")? {
             let entry = entry?;
             let mut path_type = entry.path();
@@ -26,34 +63,11 @@ impl DeviceState {
             if let "bluetooth" = fs::read_to_string(path_type)?.trim_end() {
                 let state_byte: u8 =
                     fs::read_to_string(path_state)?.trim_end().parse()?;
-                bt_state = Some(Self::from_byte(state_byte)?);
-                return Ok(bt_state);
+                bt_state_opt = Some(Self::from_byte(state_byte)?);
+                return Ok(bt_state_opt);
             }
         }
-        Ok(bt_state)
-    }
-
-    pub fn write<W: std::io::Write>(
-        &self,
-        mut buf: W,
-        prefix: &str,
-    ) -> Result<(), std::io::Error> {
-        match self {
-            Self::On => {
-                writeln!(buf, "{}on ", prefix)
-            }
-            Self::OffSoft => {
-                // TODO Distinguish from OffHard
-                writeln!(buf, "{}off", prefix)
-            }
-            Self::OffHard => {
-                // TODO Distinguish from OffSoft
-                writeln!(buf, "{}off", prefix)
-            }
-            Self::NoDev => {
-                writeln!(buf, "{}--", prefix)
-            }
-        }
+        Ok(bt_state_opt)
     }
 
     fn from_byte(b: u8) -> Result<Self> {
@@ -65,4 +79,12 @@ impl DeviceState {
             _ => Err(anyhow!("Invalid state byte: {:?}", b)),
         }
     }
+}
+
+pub fn run(prefix: &str, interval: Duration) -> Result<()> {
+    let events = crate::Clock::new(interval);
+    let event_to_msg = Box::new(|()| DeviceState::read());
+    let state = State::new(prefix);
+    let mut stdout = std::io::stdout().lock();
+    crate::pipeline(events, event_to_msg, state, &mut stdout)
 }
