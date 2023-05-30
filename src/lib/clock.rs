49f57b374becdd2334ad505e8b93e7cf45475f7a
Siraaj Khandkar <siraaj@khandkar.net>
2023-05-30 09:50:21 -0400

    Clarify note

--- src/lib/clock.rs
+++ src/lib/clock.rs
@@ -1,5 +1,5 @@
 // TODO Clock is not a good match for the semantics.
-//      We sleep only on each call, so it is more like pushback.
+//      We sleep cooperatively with the caller, so it is more like pushback.
 use std::time::Duration;
 
 pub struct Tick;
