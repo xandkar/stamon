daf8cecbd2473607b2aca17b345ef17946996ed2
Siraaj Khandkar <siraaj@khandkar.net>
2023-06-09 16:08:45 -0400

    Rename weather feed CLI flags from NOAA to NWS

--- src/bin/pista-feed-weather.rs
+++ src/bin/pista-feed-weather.rs
@@ -15,28 +15,28 @@ struct Cli {
 
     // TODO Implement summary for any Observatory, like weather::Observatory::write_summary(file)
     #[clap(long)]
-    noaa_summary_file: Option<std::path::PathBuf>,
+    nws_summary_file: Option<std::path::PathBuf>,
 
-    // TODO Can NOAA API accept coord instead of station ID?
+    // TODO Can NWS API accept coord instead of station ID?
     // TODO Can we lookup station ID by coordinates?
     // TODO Unify our CLI to accept just coordinates?
     #[clap(long)]
-    noaa_station_id: Option<String>,
+    nws_station_id: Option<String>,
 
     #[clap(long, default_value = "pista-feed-weather")]
-    noaa_app_name: String,
+    nws_app_name: String,
 
     #[clap(long, default_value = env!("CARGO_PKG_VERSION"))]
-    noaa_app_version: String,
+    nws_app_version: String,
 
-    #[clap(
-        long,
-        default_value = "https://github.com/xandkar/pista-feeds-rs"
-    )]
-    noaa_app_url: String,
+    #[clap(long, default_value = "https://github.com/xandkar/pista-feeds")]
+    nws_app_url: String,
 
+    /// Give NWS a way to contact you to inform of API misuse (interval too
+    /// short, etc), instead of just getting blocked. See "Authentication"
+    /// section at: https://www.weather.gov/documentation/services-web-api
     #[clap(long, default_value = "user-has-not-provided-contact-info")]
-    noaa_admin_email: String,
+    nws_admin_email: String,
 
     #[clap(long)]
     owm_coord: Option<owm::Coord>,
@@ -50,7 +50,7 @@ struct Cli {
 
 #[derive(Debug, Clone, Copy, ValueEnum)]
 enum ObservatoryName {
-    Noaa,
+    Nws,
     Owm,
 }
 
@@ -59,7 +59,7 @@ impl std::str::FromStr for ObservatoryName {
 
     fn from_str(s: &str) -> Result<Self> {
         match s {
-            "noaa" => Ok(Self::Noaa),
+            "nws" => Ok(Self::Nws),
             "owm" => Ok(Self::Owm),
             _ => Err(anyhow!("unknown observatory name: {:?}", s)),
         }
@@ -72,22 +72,22 @@ impl Cli {
             Vec::new();
         for o in &self.observatories {
             match o {
-                ObservatoryName::Noaa => {
+                ObservatoryName::Nws => {
                     let station_id = self
-                        .noaa_station_id
+                        .nws_station_id
                         .as_ref()
-                        .ok_or_else(|| anyhow!("missing noaa station id"))?
+                        .ok_or_else(|| anyhow!("Missing NWS station id"))?
                         .to_string();
                     let user_agent = nws::UserAgent {
-                        app_name: self.noaa_app_name.to_string(),
-                        app_version: self.noaa_app_version.to_string(),
-                        app_url: self.noaa_app_url.to_string(),
-                        admin_email: self.noaa_admin_email.to_string(),
+                        app_name: self.nws_app_name.to_string(),
+                        app_version: self.nws_app_version.to_string(),
+                        app_url: self.nws_app_url.to_string(),
+                        admin_email: self.nws_admin_email.to_string(),
                     };
                     let settings = nws::Settings {
                         station_id,
                         user_agent,
-                        summary_file: self.noaa_summary_file.clone(),
+                        summary_file: self.nws_summary_file.clone(),
                     };
                     let observatory = nws::Observatory::new(&settings)?;
                     observatories.push(Box::new(observatory));
